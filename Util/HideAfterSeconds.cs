8bbb4885dca08ccd45e49a02267fb8ce1819334b
Aaron Sarazan <aaron@sarazan.net>
2018-06-29 11:20:43 -0700

    hide after seconds

---
+++ Util/HideAfterSeconds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Plugins.Util
+{
+    public class HideAfterSeconds : MonoBehaviour
+    {
+        public float Seconds;
+
+        private void OnEnable()
+        {
+            StartCoroutine(Countdown());
+        }
+
+        private IEnumerator Countdown()
+        {
+            yield return new WaitForSeconds(Seconds);
+            if (gameObject != null)
+                gameObject.SetActive(false);   
+        }
+    }
+}
