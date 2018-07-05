9b14e2c7d662c0450c06a78cf9e891254ab1f12f
Aaron Sarazan <aaron@sarazan.net>
2018-07-05 15:28:25 -0700

    exception helpers

---
+++ Util/Exceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Plugins.Util
+{
+    public static class Exceptions
+    {
+        public static string[] AggregateMessages(this AggregateException ex)
+        {
+            return ex.InnerExceptions.Select(it => it.Message).ToArray();
+        }
+
+        public static void LogMessages(this AggregateException ex)
+        {
+            foreach (var msg in ex.AggregateMessages())
+                Debug.LogError(msg);
+        }
+        
+    }
+}
