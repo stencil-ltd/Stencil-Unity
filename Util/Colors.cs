41581e3bb104dab9aff4648f713afcbf84e7fa0e
Aaron Sarazan <aaron@sarazan.net>
2018-07-10 12:43:30 -0700

    color helpers

---
+++ Util/Colors.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Plugins.Util
+{
+    public static class Colors
+    {
+        public static Color Alpha(this Color color, byte alpha)
+        {
+            color.a = alpha;
+            return color;
+        }
+        
+        public static Color Alpha(this Color color, float alpha)
+        {
+            color.a = (byte) (byte.MaxValue * alpha);
+            return color;
+        }
+    }
+}
