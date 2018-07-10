69486e6df7dec51281e9cf60a038dc502f56cff8
Aaron Sarazan <aaron@sarazan.net>
2018-07-10 11:00:04 -0700

    formatting

--- Physics/Trajectories.cs
+++ Physics/Trajectories.cs
@@ -7,21 +7,18 @@ namespace Plugins.Physics
         // https://tech.spaceapegames.com/2016/07/05/trajectory-prediction-with-unity-physics
         public static Vector2[] Plot(Rigidbody2D rigidbody, Vector2 pos, Vector2 velocity, int steps)
         {
-            Vector2[] results = new Vector2[steps];
- 
-            float timestep = Time.fixedDeltaTime / Physics2D.velocityIterations;
-            Vector2 gravityAccel = Physics2D.gravity * rigidbody.gravityScale * timestep * timestep;
-            float drag = 1f - timestep * rigidbody.drag;
-            Vector2 moveStep = velocity * timestep;
- 
-            for (int i = 0; i < steps; ++i)
+            var results = new Vector2[steps];
+            var timestep = Time.fixedDeltaTime / Physics2D.velocityIterations;
+            var gravityAccel = Physics2D.gravity * rigidbody.gravityScale * timestep * timestep;
+            var drag = 1f - timestep * rigidbody.drag;
+            var moveStep = velocity * timestep;
+            for (var i = 0; i < steps; ++i)
             {
                 moveStep += gravityAccel;
                 moveStep *= drag;
                 pos += moveStep;
                 results[i] = pos;
             }
- 
             return results;
         }
         
