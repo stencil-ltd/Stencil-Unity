e7d48f3bcd13f55c3432d7669e9aa1e8a357d29b
Aaron Sarazan <aaron@sarazan.net>
2018-07-16 21:28:59 -0700

    fixing the damage

--- Binding/BindUtils.cs
+++ Binding/BindUtils.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
+using Plugins.State;
 using UnityEngine;
 
 namespace Binding
@@ -18,6 +20,37 @@ namespace Binding
                 .Where(prop => prop.IsDefined(attr, true));
             foreach (var prop in props)
                 prop.SetValue(obj, obj.GetComponent(prop.PropertyType));
+        }
+
+        public static void BindStates(this MonoBehaviour obj)
+        {
+            var attr = typeof(BindState);
+            var type = obj.GetType();
+            var states = Resources.FindObjectsOfTypeAll<State>();
+
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(field => field.IsDefined(attr, true));
+            foreach (var field in fields)
+            {
+                var bstate = field.GetCustomAttribute<BindState>();
+                field.SetValue(obj, Find(bstate.Name ?? field.Name, states));
+            }
+
+            var props = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(prop => prop.IsDefined(attr, true));
+            foreach (var prop in props)
+            {
+                var bstate = prop.GetCustomAttribute<BindState>();
+                prop.SetValue(obj, Find(bstate.Name ?? prop.Name, states));
+            }
+        }
+
+        private static State Find(string name, State[] states)
+        {
+            var find = states.FirstOrDefault((arg) => arg.Name.Replace(" ", "") == name);
+            if (find == null)
+                throw new Exception($"Could not find state {name}");
+            return find;
         }
     }
 }
---
+++ State/BindState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Plugins.State
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class BindState : Attribute
+    {
+        public readonly string Name;
+        public BindState(string name = null)
+        {
+            Name = name;
+        }
+    }
+}
