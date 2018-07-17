9284c0d7b14cb9af6ac0891d8c3071df87424321
Aaron Sarazan <aaron@sarazan.net>
2018-07-17 13:59:57 -0700

    ad configurations

---
+++ Ads/AdConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Util;
+
+namespace Plugins.Ads
+{
+    public class AdConfiguration
+    {
+        public readonly PlatformValue<string> UnitId;
+        public AdConfiguration(string android, string ios, string debugAndroid, string debugIos)
+        {
+            UnitId = new PlatformValue<string>()
+                .WithAndroid(android, developer: debugAndroid)
+                .WithIos(ios, developer: debugIos);
+        }
+    }
+
+    public class BannerConfiguration : AdConfiguration
+    {
+        public BannerConfiguration(string android, string ios) 
+            : base(android, 
+                ios, 
+                "ca-app-pub-3940256099942544/6300978111", 
+                "ca-app-pub-3940256099942544/2934735716")
+        {}
+    }
+    
+    public class InterstitialConfiguration : AdConfiguration
+    {
+        public InterstitialConfiguration(string android, string ios) 
+            : base(android, 
+                ios, 
+                "ca-app-pub-3940256099942544/1033173712", 
+                "ca-app-pub-3940256099942544/4411468910")
+        {}
+    }
+
+    public class RewardedConfiguration : AdConfiguration
+    {
+        public RewardedConfiguration(string android, string ios) 
+            : base(android, 
+                ios, 
+                "ca-app-pub-3940256099942544/5224354917", 
+                "ca-app-pub-3940256099942544/1712485313")
+        {}
+    }
+}
---
+++ UI/Permanent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Plugins.UI
+{
+    public class Permanent<T> : MonoBehaviour where T : Permanent<T>
+    { 
+        public static T Instance { get; private set; }
+        protected bool Valid;
+
+        protected virtual void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Valid = true;
+            Instance = (T) this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            Instance = null;
+        }
+    }
+}
