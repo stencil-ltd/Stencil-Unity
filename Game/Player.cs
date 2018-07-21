23b45b5bef734f445535cc93091362900887b122
Aaron Sarazan <aaron@sarazan.net>
2018-07-21 14:14:27 -0700

    maybe this will be useful

---
+++ Game/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Plugins.Game
+{
+    public static class Player
+    {
+        [CanBeNull] public static GameObject Instance { get; private set; }
+        [CanBeNull] public static T Get<T>() where T : Component => Instance?.GetComponent<T>();
+
+        public static event EventHandler<GameObject> OnPlayerSet;
+
+        public static void SetPlayer([CanBeNull] GameObject player)
+        {
+            Debug.Log($"Changing player from {Instance} to {player}");
+            Instance = player;
+            OnPlayerSet?.Invoke(null, Instance);
+        }
+
+        public static void ClearPlayer([CanBeNull] GameObject player)
+        {
+            if (Instance != player) return;
+            SetPlayer(null);
+        }
+    }
+}
