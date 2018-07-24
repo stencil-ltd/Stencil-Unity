d6474866445f87c811340be8176361a4202f2bfc
Aaron Sarazan <aaron@sarazan.net>
2018-07-23 22:34:53 -0700

    deques and such

---
+++ Collections/CollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Plugins.Collections
+{
+    public static class CollectionExtensions
+    {
+        public static T Front<T>(this Deque<T> deque) => deque.First();
+        public static T Back<T>(this Deque<T> deque) => deque.Last();
+    }
+}
