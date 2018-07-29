0ae47c4a3511f45fa033dfd5fc9a2f7524d55b9a
Aaron Sarazan <aaron@sarazan.net>
2018-07-29 14:56:40 -0700

    waitforanim

---
+++ Coroutines/WaitForAnim.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Plugins.Coroutines
+{
+    public class WaitForAnim : CustomYieldInstruction
+    {
+        public readonly Animator Animator;
+        public readonly string State;
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                var info = Animator.GetCurrentAnimatorStateInfo(0);
+                var isAtState = info.IsName(State);
+                var trans = Animator.IsInTransition(0);
+                return !isAtState || trans;
+            }
+        }
+
+        public WaitForAnim(Animator animator, string state)
+        {
+            Animator = animator;
+            State = state;
+        }
+    }
+}
