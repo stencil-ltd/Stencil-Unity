2fca053ec8afe9e957f397ac6813a8c604e62db9
Aaron Sarazan <aaron@sarazan.net>
2018-07-31 12:14:46 -0700

    buyables

---
+++ Scripts/Store/Buyable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using Currencies;
+using Util;
+
+namespace Store
+{
+    [CreateAssetMenu(menuName = "Buyables/Buyable")]
+    public class Buyable : ScriptableObject
+    {
+        public string Id;
+        public string Title;
+        
+        [Header("Assets")]
+        public Sprite Icon;
+        public Texture Skin;
+        public GameObject Prefab;
+        
+        [Header("Money")]
+        public Currency Currency;
+        public long Price;
+        
+        public BuyableManager Manager { get; internal set; }
+
+        public string Key => $"buyable_{Manager.Id}_{Id}";
+        
+        public event EventHandler<Buyable> OnAcquired;
+ 
+        public DateTime? DateAcquired => PlayerPrefsX.GetDateTime(Key);
+        public bool Acquired
+        {
+            get { return DateAcquired != null; }
+            set
+            {
+                if (Acquired == value) return;
+                if (value)
+                    PlayerPrefsX.SetDateTime(Key, DateTime.Now);
+                else PlayerPrefs.DeleteKey(Key);
+                PlayerPrefs.Save();
+                if (value) OnAcquired?.Invoke(this, this);
+            }
+        }
+        
+        public override string ToString()
+        {
+            return $"[{Id}] - Buyable {Title} ({Manager})";
+        }
+    }
+}
---
+++ Scripts/Store/BuyableManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Store
+{
+    [CreateAssetMenu(menuName = "Buyables/Manager")]
+    public class BuyableManager : ScriptableObject
+    {
+        private static readonly Dictionary<string, BuyableManager> Registry 
+            = new Dictionary<string, BuyableManager>();
+
+        public string Id;
+        public Buyable[] Buyables;
+        public EventHandler<Buyable> OnAcquired;
+
+        private void Awake()
+        {
+            Registry[Id] = this;
+            foreach (var b in Buyables)
+            {
+                if (b.Manager != null) throw new Exception("each buyable can have one manager.");
+                b.Manager = this;
+                b.OnAcquired += _OnAcquired;
+            }
+        }
+
+        public static BuyableManager GetManager(string id) => Registry[id];
+        public Buyable Find(string id) => Buyables.First(b => b.Id == id);
+        
+        private void _OnAcquired(object sender, Buyable e) => OnAcquired?.Invoke(sender, e);
+        
+        public override string ToString()
+        {
+            return $"Buyable Manager {Id}";
+        }
+    }
+}
