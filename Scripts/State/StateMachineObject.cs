98123e65215e42bd5b78a55a8485c28892a610df
Aaron Sarazan <aaron@sarazan.net>
2018-08-08 15:44:15 -0700

    rename state machines

---
+++ Scripts/State/StateMachineObject.cs
@@ -0,0 +1,19 @@
+﻿using Binding;
+using Plugins.UI;
+
+namespace State
+{
+    public class StateMachineObject : Permanent<StateMachineObject>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            this.BindStates();
+        }
+
+        private void Start()
+        {
+            StateMachines.Initialize();
+        }
+    }
+}
