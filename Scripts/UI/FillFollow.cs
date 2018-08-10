c09416627224f5d852beb029a47db7ff649fdb4d
Aaron Sarazan <aaron@sarazan.net>
2018-08-10 16:01:10 -0700

    fill follow

---
+++ Scripts/UI/FillFollow.cs
@@ -0,0 +1,35 @@
+﻿using Binding;
+using UnityEngine;
+
+namespace Menu.Meter
+{
+    [ExecuteInEditMode]
+    public class FillFollow : MonoBehaviour
+    {
+        public float Offset;
+        public RectTransform Follow;
+
+        [Bind]
+        private RectTransform _rect;
+
+        private void Awake()
+        {
+            this.Bind();
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            UpdateFollow();   
+        }
+
+        private void UpdateFollow()
+        {
+            var size = Follow.offsetMin.x + Offset;
+            _rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size);
+            var offset = _rect.offsetMin;
+            offset.x = 0;
+            _rect.offsetMin = offset;   
+        }
+    }
+}
