6d1bb020a2bf695665aaeec34f63f2d8fee4ee15
Aaron Sarazan <aaron@sarazan.net>
2018-08-11 14:33:34 -0700

    ftui

---
+++ Standard/Scripts/Ftui/FtuiController.cs
@@ -0,0 +1,37 @@
+﻿using Standard.States;
+using UI;
+using UnityEngine;
+
+namespace Standard.Ftui
+{
+    public class FtuiController : Controller<FtuiController>
+    {
+        private static int _ftuiIndex = 0;
+        
+        public GameObject[] Ftuis;
+        public bool RandomizeSequence = false; // alternate by default.
+
+        private void OnEnable()
+        {
+            int index;
+            if (RandomizeSequence)
+                index = Random.Range(0, Ftuis.Length);
+            else
+            {
+                index = _ftuiIndex++ % Ftuis.Length;
+            }
+            for (var i = 0; i < Ftuis.Length; i++)
+                Ftuis[i].SetActive(i == index);
+        }
+
+        private void OnDisable()
+        {
+            PlayStates.Instance.RequestState(PlayStates.State.Playing);
+        }
+
+        public static void Show()
+        {
+            PlayStates.Instance.RequestState(PlayStates.State.Loading);
+        }
+    }
+}
