0c8dc257635b5200465a981b8855c55613ffea31
Aaron Sarazan <aaron@sarazan.net>
2018-08-16 10:57:24 -0700

    weighted options

--- Scripts/Collections/CollectionExtensions.cs
+++ Scripts/Collections/CollectionExtensions.cs
@@ -9,20 +9,5 @@ namespace Plugins.Collections
     {
         public static T Front<T>(this Deque<T> deque) => deque.First();
         public static T Back<T>(this Deque<T> deque) => deque.Last();
-
-        public static T WeightedRandom<T>(this ICollection<T> coll, Func<T, float> weight)
-        {
-            var total = coll.Sum(weight);
-            var rand = Random.Range(0, total);
-            var test = 0f;
-            foreach (var p in coll)
-            {
-                var w = weight(p);
-                if (rand < w + test)
-                    return p;
-                test += w;
-            }
-            throw new Exception("Could not choose by weight. Are there any in the list?");
-        }
     }
 }
---
+++ Scripts/Util/WeightedOption.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Util
+{
+    [Serializable]
+    public abstract class WeightHaver
+    {
+        [Range(0,1)]
+        public float Weight = 1f;
+    }
+    
+    [Serializable]
+    public class WeightedOption<T> : WeightHaver
+    {
+        public T Option;
+    }
+
+    public static class WeightExtensions
+    {
+        public static T WeightedRandom<T>(this ICollection<T> coll) where T : WeightHaver
+        {
+            var total = coll.Sum(w => w.Weight);
+            var rand = Random.Range(0, total);
+            var test = 0f;
+            foreach (var p in coll)
+            {
+                var w = p.Weight;
+                if (rand < w + test)
+                    return p;
+                test += w;
+            }
+            throw new Exception("Could not choose by weight. Are there any in the list?");
+        }
+    }
+}
