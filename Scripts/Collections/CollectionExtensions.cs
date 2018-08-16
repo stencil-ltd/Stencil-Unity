08f30675f0f5c7adc8e5e16f1b783e1d04c846fd
Aaron Sarazan <aaron@sarazan.net>
2018-08-16 10:00:06 -0700

    collection stuff

--- Scripts/Collections/CollectionExtensions.cs
+++ Scripts/Collections/CollectionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Random = UnityEngine.Random;
 
 namespace Plugins.Collections
 {
@@ -7,5 +9,20 @@ namespace Plugins.Collections
     {
         public static T Front<T>(this Deque<T> deque) => deque.First();
         public static T Back<T>(this Deque<T> deque) => deque.Last();
+
+        public static T WeightedRandom<T>(this ICollection<T> coll, Func<T, float> weight)
+        {
+            var total = coll.Sum(weight);
+            var rand = Random.Range(0, total);
+            var test = 0f;
+            foreach (var p in coll)
+            {
+                var w = weight(p);
+                if (rand < w + test)
+                    return p;
+                test += w;
+            }
+            throw new Exception("Could not choose by weight. Are there any in the list?");
+        }
     }
 }
