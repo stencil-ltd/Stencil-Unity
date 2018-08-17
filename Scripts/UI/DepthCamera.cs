8734831f3bfa423130d9024207af037d67c6e8b1
Aaron Sarazan <aaron@sarazan.net>
2018-08-17 11:49:34 -0700

    depthcamera

---
+++ Scripts/UI/DepthCamera.cs
@@ -0,0 +1,19 @@
+﻿using Binding;
+using UnityEngine;
+
+namespace UI
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    public class DepthCamera : MonoBehaviour
+    {
+        [Bind] 
+        private Camera _cam;
+
+        private void Start()
+        {
+            this.Bind();
+            _cam.depthTextureMode = DepthTextureMode.Depth;
+        }
+    }
+}
