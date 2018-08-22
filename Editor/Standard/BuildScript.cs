3ee3654c709b0feff69c6d9f7c573db5418d8ce6
Aaron Sarazan <aaron@sarazan.net>
2018-08-21 19:27:01 -0700

    update namespace

--- Editor/Standard/BuildScript.cs
+++ Editor/Standard/BuildScript.cs
@@ -1,35 +1,32 @@
 ﻿using System.Linq;
 using UnityEditor;
 
-namespace Standard
-{
-    public class BuildScript {
+public class BuildScript {
 
-        public static void PerformAndroidBuild()
-        { 
-            Build(BuildTarget.Android);
-        }
-    
-        public static void PerformiOSBuild()
+    public static void PerformAndroidBuild()
+    { 
+        Build(BuildTarget.Android);
+    }
+
+    public static void PerformiOSBuild()
+    {
+        Build(BuildTarget.iOS);
+    }
+
+    public static void Build(BuildTarget target)
+    {
+        var levels = EditorBuildSettings.scenes.ToArray();
+        var suffix = "";
+        switch (target)
         {
-            Build(BuildTarget.iOS);
+            case BuildTarget.Android:
+                suffix = ".apk";
+                break;
+            default:
+                suffix = "";
+                break;
         }
-    
-        public static void Build(BuildTarget target)
-        {
-            var levels = EditorBuildSettings.scenes.ToArray();
-            var suffix = "";
-            switch (target)
-            {
-                case BuildTarget.Android:
-                    suffix = ".apk";
-                    break;
-                default:
-                    suffix = "";
-                    break;
-            }
-            var path = $"Builds/{target}{suffix}";
-            BuildPipeline.BuildPlayer(levels, path, target, BuildOptions.None);
-        }
+        var path = $"Builds/{target}{suffix}";
+        BuildPipeline.BuildPlayer(levels, path, target, BuildOptions.None);
     }
 }
