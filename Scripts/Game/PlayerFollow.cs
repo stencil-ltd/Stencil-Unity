6576cd43f33e34dcc04d481c4388c4d8591fe3b0
Aaron Sarazan <aaron@sarazan.net>
2018-08-21 23:31:29 -0700

    playerfollow

---
+++ Scripts/Game/PlayerFollow.cs
@@ -0,0 +1,15 @@
+﻿using UI;
+using UnityEngine;
+
+namespace Game
+{
+    public class PlayerFollow : Controller<PlayerFollow>
+    {
+        private void LateUpdate()
+        {
+            var player = Player.Instance;
+            if (player != null)
+                transform.position = player.transform.position;
+        }
+    }
+}
