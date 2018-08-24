2d9a600222f03504b4ccfd11ff4484b5b0d80405
Aaron Sarazan <aaron@sarazan.net>
2018-08-24 14:59:59 -0700

    easy sound and such

---
+++ Scripts/Audio/EasySound.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using Binding;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace UI
+{
+    [RequireComponent(typeof(AudioSource))]
+    [ExecuteInEditMode]
+    public class EasySound : MonoBehaviour
+    {
+        public static AudioMixer Mixer;
+        
+        public static AudioMixerGroup Master;
+        public static AudioMixerGroup Music;
+        public static AudioMixerGroup Sfx;
+
+        public SoundType Type = SoundType.Sfx;
+        public AudioClip Clip;
+        
+        [Bind] 
+        public AudioSource Source { get; private set; }
+
+        [RuntimeInitializeOnLoadMethod]
+        public static void OnLoad()
+        {
+            var mixers = Resources.FindObjectsOfTypeAll<AudioMixer>();
+            Mixer = mixers.First(mixer => mixer.name == "Mixer");
+            Sfx = Mixer.FindMatchingGroups("Sfx")[0];
+            Music = Mixer.FindMatchingGroups("Music")[0];
+            Master = Mixer.FindMatchingGroups("Master")[0];
+        }
+
+        private void Awake()
+        {
+            this.Bind();
+            UpdateSource();
+            
+            #if UNITY_EDITOR
+            if (!Application.isPlaying)
+                InternalEditorUtility.SetIsInspectorExpanded(Source, false);
+            #endif
+        }
+        
+        #if UNITY_EDITOR
+        private void Update()
+        {
+            if (Application.isPlaying)
+                UpdateSource();
+        }
+
+        private void UpdateSource()
+        {
+            Source.clip = Clip;
+            switch (Type)
+            {
+                case SoundType.Sfx:
+                    Source.outputAudioMixerGroup = Sfx;
+                    break;
+                case SoundType.Music:
+                    Source.outputAudioMixerGroup = Music;
+                    break;
+                case SoundType.None:
+                    Source.outputAudioMixerGroup = null;
+                    break;
+            }
+        }
+#endif
+    }
+
+    [Serializable]
+    public enum SoundType
+    {
+        Sfx, Music, None
+    }
+}
---
+++ Scripts/Audio/PlayOnCollision.cs
@@ -0,0 +1,44 @@
+﻿using Binding;
+using UI;
+using UnityEngine;
+
+namespace Audio
+{
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(EasySound))]
+    [ExecuteInEditMode]
+    public class PlayOnCollision : MonoBehaviour
+    {
+        public bool IsTrigger;
+        public string ColliderTag;
+        
+        [Bind] public Collider Collider { get; private set; }
+        [Bind] public EasySound Sound { get; private set; }
+
+        private void Awake()
+        {
+            this.Bind();
+        }
+
+        private void Start()
+        {
+            Sound.Source.playOnAwake = false;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (IsTrigger) return;
+            if (!string.IsNullOrEmpty(ColliderTag) && !other.gameObject.CompareTag(ColliderTag)) return;
+            Debug.Log($"Playing {Sound.Clip}");
+            Sound.Source.Play();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!IsTrigger) return;
+            if (!string.IsNullOrEmpty(ColliderTag) && !other.gameObject.CompareTag(ColliderTag)) return;
+            Debug.Log($"Playing {Sound.Clip}");
+            Sound.Source.Play();
+        }
+    }
+}
