6572db140d5391c7bf83329933ba8b95b3d1cba3
Aaron Sarazan <aaron@sarazan.net>
2018-09-05 14:39:01 -0700

    screenshots colors and menus

---
+++ Standard/Scripts/Menu/ScreenshotZone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Standard.Menu
+{
+    public class ScreenshotZone : Controller<ScreenshotZone>
+    {
+        public Text ScoreText;
+        public void SetScore(int score)
+        {
+            ScoreText.text = $"{score:N0}";
+        }
+    }
+}
