c802f790e963dc0cdaa35529fd310810615fd1c5
Aaron Sarazan <aaron@sarazan.net>
2018-09-05 12:12:52 -0700

    update build script

--- Editor/Standard/BuildScript.cs
+++ Editor/Standard/BuildScript.cs
@@ -5,6 +5,25 @@ using UnityEngine;
 
 public class BuildScript {
 
+    [MenuItem("Stencil/Build/Increment Version")]
+    public static void IncrementVersion()
+    {
+        PlayerSettings.Android.bundleVersionCode++;
+        PlayerSettings.iOS.buildNumber = "" + (int.Parse(PlayerSettings.iOS.buildNumber) + 1);
+    }
+    
+    [MenuItem("Stencil/Build/Both")]
+    public static void PerformBothBuild()
+    {
+#if UNITY_ANDROID
+        PerformAndroidBuild();
+        PerformiOSBuild();
+#elif UNITY_IOS
+        PerformiOSBuild();
+        PerformAndroidBuild();
+#endif
+    }
+    
     [MenuItem("Stencil/Build/Android")]
     public static void PerformAndroidBuild()
     { 
@@ -16,43 +35,15 @@ public class BuildScript {
     {
         Build(BuildTarget.iOS);
     }
-    
-    [MenuItem("Stencil/Build/Both")]
-    public static void PerformBothBuild()
-    {
-#if UNITY_ANDROID
-        Build(BuildTarget.Android);
-        Build(BuildTarget.iOS);
-#elif UNITY_IOS
-        Build(BuildTarget.iOS);
-        Build(BuildTarget.Android);
-        #endif
-    }
 
     public static void Build(BuildTarget target)
     {
         var levels = EditorBuildSettings.scenes.ToArray();
-        var suffix = "";
-        switch (target)
-        {
-            case BuildTarget.Android:
-                suffix = ".apk";
-                break;
-            default:
-                suffix = "";
-                break;
-        }
-
-        var path = $"Builds/{target}{suffix}";
+        var path = $"Builds/{target}";
         var dir = $"{Application.dataPath}/../";
         var abspath = dir + path;
-        if (File.Exists(abspath))
-        {
-            var bk = $"{abspath}.bk";
-            if (File.Exists(bk))
-                File.Delete(bk);
-            File.Move(abspath, bk);
-        }
+        if (Directory.Exists(abspath))
+            Directory.Delete(abspath, true);
         var dev = EditorUserBuildSettings.development ? BuildOptions.Development : BuildOptions.None;
         BuildPipeline.BuildPlayer(levels, path, target, dev);
     }
