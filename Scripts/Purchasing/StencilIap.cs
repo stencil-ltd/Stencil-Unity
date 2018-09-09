05d00430e01c05efeb4a79a963f8469890428c09
Aaron Sarazan <aaron@sarazan.net>
2018-09-09 16:13:34 -0700

    stencil-iap

---
+++ Scripts/Purchasing/StencilIap.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine.Purchasing;
+
+namespace Purchasing
+{
+    public static class StencilIap
+    {
+        #if UNITY_PURCHASING
+        public static bool IsReady()
+        {
+            return CodelessIAPStoreListener.initializationComplete;
+        }
+        
+        [CanBeNull]
+        public static Product GetProduct(this IAPButton button)
+        {
+            return CodelessIAPStoreListener.Instance.GetProduct(button.productId);
+        }
+
+        [CanBeNull]
+        public static PayoutDefinition GetPayout(this ProductDefinition def, string name)
+        {
+            return def.payouts?.FirstOrDefault(definition => definition.subtype == name);
+        }
+        #endif
+    }
+}
