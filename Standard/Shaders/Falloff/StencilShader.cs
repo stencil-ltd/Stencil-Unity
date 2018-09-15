3c6809e28e79b88abb04cfa0dbe75e385e49e69f
Aaron Sarazan <aaron@sarazan.net>
2018-09-14 20:08:08 -0700

    unified stencil shader script

--- Standard/Shaders/Falloff/StencilShader.cs
+++ Standard/Shaders/Falloff/StencilShader.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 namespace Standard.Shaders.Falloff
 {
     [RequireComponent(typeof(MaterialCollector))]
-    public class DistanceDropoff : MonoBehaviour
+    public class StencilShader : MonoBehaviour
     {
+        public bool DistanceFade;
+        public bool HeightFade;
+        public bool DistanceDropoff;
+    
         [Bind] private MaterialCollector _materials;
 
         private void Awake()
@@ -19,7 +23,9 @@ namespace Standard.Shaders.Falloff
             {
                 var prop = new MaterialPropertyBlock();
                 r.GetPropertyBlock(prop);
-                prop.SetInt("_DistanceDropoff", 1);
+                prop.SetInt("_UseDistance", DistanceFade ? 1 : 0);
+                prop.SetInt("_UseHeight", HeightFade ? 1 : 0);
+                prop.SetInt("_DistanceDropoff", DistanceDropoff ? 1 : 0);
                 r.SetPropertyBlock(prop);
             }
         }
