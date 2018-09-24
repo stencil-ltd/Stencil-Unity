92ce6d485d4eb12b29d837055fe85580b0e12fa3
Aaron Sarazan <aaron@sarazan.net>
2018-09-24 13:42:54 -0700

    accessors for velocity

--- Scripts/Physic/ConstantVelocity.cs
+++ Scripts/Physic/ConstantVelocity.cs
@@ -5,7 +5,25 @@ namespace Physic
     public class ConstantVelocity : MonoBehaviour
     {
         public Vector3 Velocity;
-        
+
+        public float x
+        {
+            get { return Velocity.x; }
+            set { Velocity.x = value; }
+        }
+
+        public float y
+        {
+            get { return Velocity.y; }
+            set { Velocity.y = value; }
+        }
+
+        public float z
+        {
+            get { return Velocity.z; }
+            set { Velocity.z = value; }
+        }
+
         private void FixedUpdate()
         {
             var pos = transform.position;
