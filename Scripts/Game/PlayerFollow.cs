224e5d0ac26dbf590f7b61ef71a904109bdbfe17
Aaron Sarazan <aaron@sarazan.net>
2018-09-26 12:19:18 -0700

    expand playerFollow

--- Scripts/Game/PlayerFollow.cs
+++ Scripts/Game/PlayerFollow.cs
@@ -1,11 +1,37 @@
-﻿using UI;
-using UnityEngine;
+﻿using System;
+using UI;
 
 namespace Game
 {
     public class PlayerFollow : Controller<PlayerFollow>
     {
+        [Serializable]
+        public enum UpdateStage
+        {
+            Update, FixedUpdate, LateUpdate
+        }
+
+        public UpdateStage Stage = UpdateStage.LateUpdate;
+
+        private void Update()
+        {
+            if (Stage == UpdateStage.Update)
+                Follow();
+        }
+
+        private void FixedUpdate()
+        {
+            if (Stage == UpdateStage.FixedUpdate)
+                Follow();
+        }
+
         private void LateUpdate()
+        {
+            if (Stage == UpdateStage.LateUpdate)
+                Follow();
+        }
+
+        private void Follow()
         {
             var player = Player.Instance;
             if (player != null)
