234211e48690e292ae33322c08796244f3860422
Aaron Sarazan <aaron@sarazan.net>
2018-09-28 15:14:28 -0700

    colors are wack

--- Scripts/Util/Colors.cs
+++ Scripts/Util/Colors.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Plugins.Util
 {
@@ -9,15 +11,25 @@ namespace Plugins.Util
             return $"#{(byte)(color.r * 255):X2}{(byte)(color.g * 255):X2}{(byte)(color.b * 255):X2}";
         }
 
+        [Obsolete("This is fucking wrong")]
         public static Color Alpha(this Color color, byte alpha)
         {
             color.a = alpha;
             return color;
         }
         
+        [Obsolete("This is fucking wrong")]
         public static Color Alpha(this Color color, float alpha)
         {
             color.a = (byte) (byte.MaxValue * alpha);
+            return color;
+        }
+
+        public static Color Alpha(this Image image, float alpha)
+        {
+            var color = image.color;
+            color.a = alpha;
+            image.color = color;
             return color;
         }
     }
