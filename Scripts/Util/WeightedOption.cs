20ebd92928064a23523fcebae9332c1ff61d89bb
Aaron Sarazan <aaron@sarazan.net>
2018-10-17 18:39:10 +0000

    iweightahver

--- Scripts/Util/WeightedOption.cs
+++ Scripts/Util/WeightedOption.cs
@@ -7,11 +7,17 @@ using Random = UnityEngine.Random;
 
 namespace Util
 {
+    public interface IWeightHaver
+    {
+        float GetWeight();
+    }
+    
     [Serializable]
-    public class WeightHaver
+    public class WeightHaver : IWeightHaver
     {
         [Range(0,1)]
         public float Weight = 1f;
+        public float GetWeight() => Weight;
 
         public WeightHaver()
         {
@@ -26,20 +32,20 @@ namespace Util
     public static class WeightExtensions
     {
         [CanBeNull]
-        public static T WeightedRandom<T>(this ICollection<T> coll) where T : WeightHaver
+        public static T WeightedRandom<T>(this ICollection<T> coll, float? total = null) where T : IWeightHaver
         {
-            var total = coll.Sum(w => w.Weight);
-            var rand = Random.Range(0f, total);
+            total = total ?? coll.Sum(w => w.GetWeight());
+            var rand = Random.Range(0f, total.Value);
             var test = 0f;
             foreach (var p in coll)
             {
-                var w = p.Weight;
-                if (rand < w + test)
+                var w = p.GetWeight();
+                if (rand >= test && rand < w + test)
                     return p;
                 test += w;
             }
 
-            return null;
+            return default(T);
         }
     }
 }
