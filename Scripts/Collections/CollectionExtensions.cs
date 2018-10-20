c8cb964af4952c9f3d60902a405cc6d6b29a7bd3
Aaron Sarazan <aaron@sarazan.net>
2018-10-20 12:04:38 -0700

    shuffle

--- Scripts/Collections/CollectionExtensions.cs
+++ Scripts/Collections/CollectionExtensions.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Random = UnityEngine.Random;
 
 namespace Plugins.Collections
 {
     public static class CollectionExtensions
     {
+        private static Random rng = new Random();
+        
         public static T Front<T>(this Deque<T> deque) => deque.First();
         public static T Back<T>(this Deque<T> deque) => deque.Last();
 
@@ -14,6 +15,18 @@ namespace Plugins.Collections
         {
             var idx = UnityEngine.Random.Range(0, coll.Count);
             return coll[idx];
+        }
+        
+        public static void Shuffle<T>(this IList<T> list)  
+        {  
+            var n = list.Count;  
+            while (n > 1) {  
+                n--;  
+                var k = rng.Next(n + 1);  
+                var value = list[k];  
+                list[k] = list[n];  
+                list[n] = value;  
+            }  
         }
     }
 }
