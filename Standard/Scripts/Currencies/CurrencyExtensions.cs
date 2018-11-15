ab05dd504608eb5c020b268cbb6bd0aff83085ac
Aaron Sarazan <aaron@sarazan.net>
2018-11-14 20:36:00 -0800

    setting up rewarded ads and cash

--- Standard/Scripts/Currencies/CurrencyExtensions.cs
+++ Standard/Scripts/Currencies/CurrencyExtensions.cs
@@ -7,5 +7,9 @@ namespace Standard.Currencies
         public static Currency Coins(this CurrencyController mgr) => mgr.Get("Coins");
         public static Currency Cash(this CurrencyController mgr) => mgr.Get("Cash");
         public static Currency Wrenches(this CurrencyController mgr) => mgr.Get("Wrenches");
+        
+        public static Currency Coins(this CurrencyManager mgr) => mgr.Get("Coins");
+        public static Currency Cash(this CurrencyManager mgr) => mgr.Get("Cash");
+        public static Currency Wrenches(this CurrencyManager mgr) => mgr.Get("Wrenches");
     }
 }
