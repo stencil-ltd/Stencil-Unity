21cd7e505b82024d38a4b9c6cb44e708bc29bede
Aaron Sarazan <aaron@sarazan.net>
2018-11-14 21:36:33 -0800

    rewards and iap

---
+++ Scripts/Rewards/PurchaseRewardHandler.cs
@@ -0,0 +1,33 @@
+using AnalyticsNg;
+using Dev;
+using Lobbing;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.UDP;
+using Price = Currencies.Price;
+using Tracking = Analytics.Tracking;
+
+namespace Rewards
+{
+    public class PurchaseRewardHandler : MonoBehaviour
+    {
+        public Price Reward;
+        public Lobber Lobber;
+
+        public Transform From;
+
+        public void OnPurchaseSuccess(Product product)
+        {
+            var overrides = new LobOverrides
+            {
+                From = From
+            };
+            StartCoroutine(Lobber.LobCurrency(Reward.Currency, Reward.Amount, overrides));
+        }
+
+        public void OnPurchaseFailed(Product product, PurchaseFailureReason reason)
+        {
+            Tracking.Record($"Purchase failed {product} ({reason})");
+        }
+    }
+}
