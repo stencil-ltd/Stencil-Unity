7c2eecc638c6f1389fba7ffbbee5cb9f0b1602b8
Aaron Sarazan <aaron@sarazan.net>
2018-11-30 16:08:28 -0800

    Car locking

--- Scripts/Prefs/StencilPrefsExtensions.cs
+++ Scripts/Prefs/StencilPrefsExtensions.cs
@@ -8,5 +8,21 @@ namespace Scripts.Prefs
         {
             return PrefHolders.Get(prefs.config.Process(key), defaultValue);
         }
+
+        public static int Increment(this PrefHolder<int> holder, int amount = 1)
+        {
+            var current = holder.Get();
+            current += amount;
+            holder.Set(current);
+            return current;
+        }
+
+        public static int Decrement(this PrefHolder<int> holder, int amount = 1)
+        {
+            var current = holder.Get();
+            current -= amount;
+            holder.Set(current);
+            return current;
+        }
     }
 }
