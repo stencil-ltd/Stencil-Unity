614a3207e76161be977aa0c130f8fc041c4b8703
Aaron Sarazan <aaron@sarazan.net>
2018-12-15 15:52:27 -0800

    material stuff

--- Standard/Shaders/MaterialCollector.cs
+++ Standard/Shaders/MaterialCollector.cs
@@ -14,7 +14,7 @@ namespace Standard.Shaders
             get
             {
                 if (_materials == null)
-                    _materials = Renders.SelectMany(m => m.materials).ToArray();
+                    _materials = Renders.SelectMany(m => m?.materials).Where(material => material != null).ToArray();
                 return _materials;
             }
         }
@@ -27,5 +27,30 @@ namespace Standard.Shaders
         }
 
         public Renderer[] GetRenders() => GetComponentsInChildren<Renderer>(true);
+
+        #region Shader Setters
+
+        public MaterialCollector SetShader(Shader shader)
+        {
+            foreach (var mat in Materials) 
+                mat.shader = shader;
+            return this;
+        }
+
+        public MaterialCollector SetFloat(string name, float value)
+        {
+            foreach (var mat in Materials) 
+                mat.SetFloat(name, value);
+            return this;
+        }
+
+        public MaterialCollector SetColor(string name, Color value)
+        {
+            foreach (var mat in Materials) 
+                mat.SetColor(name, value);
+            return this;
+        }
+
+        #endregion
     }
 }
