4a61bb4b6d890ebc8e495962fd3239628b677c94
Aaron Sarazan <aaron@sarazan.net>
2018-12-29 10:15:46 -0800

    add follow script

---
+++ Scripts/Game/Follow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class Follow : MonoBehaviour
+    {
+        public Transform target;
+
+        private Vector3 _offset;
+
+        private void Awake()
+        {
+            _offset = target.position - transform.position;
+        }
+
+        private void LateUpdate()
+        {
+            transform.position = target.position - _offset;
+        }
+    }
+}
