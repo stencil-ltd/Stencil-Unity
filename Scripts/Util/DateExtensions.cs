cd78ce900667caa4b7e685e00fd5eb49903a9986
Aaron Sarazan <aaron@sarazan.net>
2019-01-23 16:53:25 -0800

    date extensions

--- Scripts/Util/DateExtensions.cs
+++ Scripts/Util/DateExtensions.cs
@@ -5,5 +5,13 @@ namespace Scripts.Util
     public static class DateExtensions
     {
         public static string ShortDateString(this DateTime dateTime) => $"{dateTime:MMddyyyy}";
+
+        public static DateTime GetNext(this DayOfWeek day)
+        {
+            var start = DateTime.Today.AddDays(1);
+            // The (... + 7) % 7 ensures we end up with a value in the range [0, 6]
+            int daysToAdd = ((int) day - (int) start.DayOfWeek + 7) % 7;
+            return start.AddDays(daysToAdd);
+        }
     }
 }
