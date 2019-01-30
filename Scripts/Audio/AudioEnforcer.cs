78bd1dcb4448e10bf28550152a935e568f157322
Aaron Sarazan <aaron@sarazan.net>
2019-01-30 10:44:56 -0800

    audioenforcer ugprade

--- Scripts/Audio/AudioEnforcer.cs
+++ Scripts/Audio/AudioEnforcer.cs
@@ -1,9 +1,15 @@
-using UnityEngine;
+using UI;
 
 namespace Scripts.Audio
 {
-    public class AudioEnforcer : MonoBehaviour
+    public class AudioEnforcer : RegisterableBehaviour
     {
+        public override void DidRegister()
+        {
+            base.DidRegister();
+            AudioSystem2.Instance.UpdateMixers();
+        }
+
         private void Awake()
         {
             AudioSystem2.Instance.UpdateMixers();
