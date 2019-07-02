2fcb68af4e006e8fbc048e87cfb4d2033401125c
Aaron Sarazan <aaron@sarazan.net>
2019-07-02 11:39:19 -0700

    stenciltenjin

--- Scripts/Rewards/PurchaseRewardHandler.cs
+++ Scripts/Rewards/PurchaseRewardHandler.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 using UnityEngine.Purchasing;
 using Price = Currencies.Price;
 using Tracking = Analytics.Tracking;
+            
+#if STENCIL_TENJIN
+using Scripts.Tenjin;
+#endif
 
 namespace Rewards
 {
@@ -20,6 +24,10 @@ namespace Rewards
                 From = From
             };
             StartCoroutine(Lobber.LobCurrency(Reward.Currency, (ulong) Reward.GetAmount(), overrides));
+            
+            #if STENCIL_TENJIN
+            StencilTenjin.Instance.OnProcessPurchase(product);
+            #endif
         }
 
         public void OnPurchaseFailed(Product product, PurchaseFailureReason reason)
