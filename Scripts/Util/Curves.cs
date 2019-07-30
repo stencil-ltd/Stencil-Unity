92257a8c586316d3a89c39af8fa4b81d6ca1d856
Aaron Sarazan <aaron@sarazan.net>
2019-07-30 10:47:35 -0700

    More refactors

--- Scripts/Util/Curves.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Util
-{
-    public static class Curves
-    {
-        public static readonly AnimationCurve EaseInOut = AnimationCurve.EaseInOut(0, 0, 1, 1);
-    }
-}
